1c34b912ac233a5395d6f21943a97196c4d7de22
Samuel <smlofoegbu@gmail.com>
2022-09-08 16:10:24 -0700

    data sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int s;
-	char p;
-	long int d;
-	long long int a;
-	float z;
+int s;
+char p;
+long int d;
+long long int a;
+float z;
 
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(s));
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(p));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
-	return (0);
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(s));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(p));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
+return (0);
 }
 
