22cf1bfbeb274ecba3e9bd70d02c4dbbf88857af
Samuel <smlofoegbu@gmail.com>
2022-09-08 17:52:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main entry point
+ * main - Entry point
  *
- * return: fails if (1)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-printf("and that piece of art is useful - Dora Korpar, 2015-10-19");
+printf("and that piece of art is useful"" - Dora Korpar, 2015-10-19");
 return (1);
 }
