e17ce1b96b365d9fdc88f46bc33248ed14f4a9fd
Samuel <smlofoegbu@gmail.com>
2022-09-08 17:37:19 -0700

    a quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <std.io>
+/**
+ * main entry point
+ *
+ * return: fails if (1)
+ */
+int main(void)
+{
+	write("and that piece of art is useful - Dora Korpar, 2015-10-19");
+		return (1);
+}
