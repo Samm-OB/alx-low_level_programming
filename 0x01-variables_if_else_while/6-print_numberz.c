b4e9f721de617355e2ca0adf2ea0f8a8f5465596
Samuel <smlofoegbu@gmail.com>
2022-09-10 03:03:38 -0700

    counting in another way

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int b;
 
-	for (b = '0'; b < '10'; b++)
+	for (b = 0; b <= 9; b++)
 	{
-		putchar(b);
+		putchar("%i", b);
 	}
-	putchar('\n');
+	putchar("\n");
 	return (0);
 }
