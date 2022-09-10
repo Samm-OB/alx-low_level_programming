018395a209fbc9975cb5243c91f734966ea1d1fe
Samuel <smlofoegbu@gmail.com>
2022-09-10 01:08:11 -0700

    alphabet with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print letters except e and q
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
