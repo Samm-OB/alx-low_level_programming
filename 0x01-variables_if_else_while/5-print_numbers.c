96af9abf9320f4b1c24a56eecaf0ed5aea30a98b
Samuel <smlofoegbu@gmail.com>
2022-09-10 01:23:33 -0700

    counting in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * counting in base 10
+ *
+ * return 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		printf("%i", a);
+	}
+	printf("\n");
+	return (0);
+}
