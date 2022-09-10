a9bd3275532ed69afda6c1dd1b722b03adfe1ca6
Samuel <smlofoegbu@gmail.com>
2022-09-10 15:20:51 -0700

    3 digit combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * three digit combinations
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		for (b = '1' ; b <= '9' ; b++)
+		{
+			for (c = '2' ; c <= '9' ; c++)
+			{
+				
+			}
+		}
+	}
+}
+
