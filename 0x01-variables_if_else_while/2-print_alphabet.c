639679531efefa3eb0dfc346c96baba81215b67f
Samuel <smlofoegbu@gmail.com>
2022-09-10 00:20:59 -0700

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-/**main - Entry  point
+
+/**
+ * main - Entry  point
  * to list all alphabets
  *
  * Return: always success 0
  */
-int main()
+
+int main(void)
 {
 	char ch;
 
-	for(ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
+		putchar('\n');
 		
 	}
 	return (0);
