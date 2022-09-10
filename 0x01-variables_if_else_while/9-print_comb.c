f86ce092ae30e29e4181de1d3a403d245972b0a8
Samuel <smlofoegbu@gmail.com>
2022-09-10 05:06:07 -0700

    single number combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * combination of single numbers
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int no;
+
+	for (no = '0' ; no <= '9' ; no++)
+	{
+		putchar(no);
+		if (no != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
