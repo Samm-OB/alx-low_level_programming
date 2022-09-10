3ef7e4c0bffed182d041c72d5448000054ea9178
Samuel <smlofoegbu@gmail.com>
2022-09-10 01:56:29 -0700

    count in another way

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * count in base 10 in a different code
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n')
+	return (0);
+}
