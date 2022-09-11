30953b7536522dee30a3b61e37d0fd3ec5d85f3a
Samuel <smlofoegbu@gmail.com>
2022-09-11 05:52:20 -0700

    double digit combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,18 +13,18 @@ int main(void)
 	int b;
 	int c;
 
-	for (a = '0' ; a <= '9' ; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = '1' ; b <= '9' ; b++)
+		for (b = 1; b <= 9; b++)
 		{
-			for (c = '2' ; c <= '9' ; c++)
+			for (c = 2; c <= 9; c++)
 			{
 				if (c > b && b > a)
 				{
 					putchar(a + '0');
 					putchar(b + '0');
-					putcahr(c + '0');
-					if (c != '7')
+					putchar(c + '0');
+					if (c != 7)
 					{
 						putchar(',');
 						putchar(' ');
