1bc27490fc6c9affdf866b09d0ad88d6becdcf67
Samuel <smlofoegbu@gmail.com>
2022-09-13 13:10:17 -0700

    using multiple mains to test positive, negative or zero integers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * testthe function if the integer is positive, negative or zero
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
