823d1a6e2c689f45a0bc48f92423ff2f6d3ea51f
Samuel <smlofoegbu@gmail.com>
2022-09-14 13:18:15 -0700

    printing putchar differently

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * manin - entry point
+ * prints putchar
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
