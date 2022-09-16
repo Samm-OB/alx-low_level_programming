379c3bb72791c7cab1bad6c8632e6425d7311b68
Samuel <smlofoegbu@gmail.com>
2022-09-16 16:37:54 -0700

    corrections

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * MAIN - HEADER
+ * int - prototype int
+ * void prototype void
+ */
 
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
