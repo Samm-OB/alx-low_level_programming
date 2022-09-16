7d5a98e15e60b02f10ecbd4c55266523835de491
Samuel <smlofoegbu@gmail.com>
2022-09-16 06:54:39 -0700

    isupper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,21 +2,18 @@
 
 /**
  * _isupper - checks for uppercase letters
- * @c - used to for the arguement of the function
+ * @c - int used to for the arguement of the function
  * Return: 1(success)
- * Return: 0(fail)
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' || c <= 'Z')
 	{
-		_putchar(c);
 		return (1);
 	}
 	else
 	{
-		_putchar(c);
 		return (0);
 	}
 }
