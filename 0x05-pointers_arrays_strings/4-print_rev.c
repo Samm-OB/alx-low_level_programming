00526fd54ef10b34a675e9e96dcbaba895975e2b
Samuel <smlofoegbu@gmail.com>
2022-09-19 14:36:39 -0700

    3-puts and reverse strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prins a string in reverse
+ * @s: reverse string
+ * Return: no error
+ */
+
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+	{
+		putchar(*(s + len));
+	}
+	putchar(10);
+}
