8ffff22cbb988956242b5830eebc8ffddaaddcd7
Samuel <smlofoegbu@gmail.com>
2022-09-20 14:17:08 -0700

    password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,51 @@
+#include <maths.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void randomPasswordGeneration(int n)
+{
+	int i = 0;
+
+	int randomizer = 0;
+
+	srand((unsigned int)(time NULL));
+
+	char numbers[] = "0123456789";
+
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+
+	char upperCase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	char symbols[] = "!@#$%^&*";
+	
+	randomizer = rand() % 4;
+
+	for (i = 0; i < n; i++)
+	{
+		if  (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = symbols[rand() % 8];
+			randomizer = rand % 4;
+			printf("%c",password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = upperCase[rand() % 26];
+			randomizer = rand % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letters[rand() % 26];
+			randomizer = rand % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
