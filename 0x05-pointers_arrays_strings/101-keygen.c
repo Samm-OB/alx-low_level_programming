49dd76e75add054ceb8114ef8706ada5c9b24456
Samuel <smlofoegbu@gmail.com>
2022-09-20 14:26:00 -0700

    random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <maths.h>
 
 void randomPasswordGeneration(int n)
 {
@@ -47,4 +48,13 @@ void randomPasswordGeneration(int n)
 			printf("%c", password[i]);
 		}
 	}
+}
+
+int main()
+{
+	int n = 10;
+
+	randomPasswordGeneration(n);
+
+	return (0);
 }
