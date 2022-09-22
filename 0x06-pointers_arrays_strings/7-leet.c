e0f855f98564260cf4d627880c1a754cd136a6be
Samuel <smlofoegbu@gmail.com>
2022-09-22 13:15:40 -0700

    leet code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 char *leet(char *x)
 {
 	int a = 0, b = 0, c = 5;
-	char tr[5] = {'A', 'E', 'I', 'O', 'U'};
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', '1'};
 
 	while (x[a])
