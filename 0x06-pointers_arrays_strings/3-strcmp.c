33e4ae0ebf38e88662cc47fffe96ce1e23c2f4eb
Samuel <smlofoegbu@gmail.com>
2022-09-22 03:36:48 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,10 +5,17 @@
  * _strcmp - function to compare strings
  * @s1: string one
  * @s2: string two
- * Return: string
+ * Return: 1(success)
  */
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
-	return (0);
+	value = strcmp(s1, s2);
+	if (value == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
