adccb0d845a1cb3476728f5375d04b2c66753956
Samuel <smlofoegbu@gmail.com>
2022-09-23 04:50:03 -0700

    print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - function to print integer
+ * @n: parameter
+ * Return: 0 success
+ */
+
+void print_number(int n)
+{
+	printf("enter an integer: ")
+
+	scanf("%d", &n);
+	_putchar(n);
+	return (0);
+}
