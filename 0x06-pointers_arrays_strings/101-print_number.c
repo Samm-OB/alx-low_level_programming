68986054e67544752f08e2842f21ee098380cebd
Samuel <smlofoegbu@gmail.com>
2022-09-23 05:16:20 -0700

    fix 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_number - function to print integer
@@ -9,7 +8,7 @@
 
 void print_number(int n)
 {
-	int n;
+	int n = n;
 
 	printf("enter an integer: ")
 	{
