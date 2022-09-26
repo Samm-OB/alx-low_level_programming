a6154d155ad6a6aa9c2a8f5aa8843576e30bc139
Samuel <smlofoegbu@gmail.com>
2022-09-26 14:15:07 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,9 +10,12 @@ void print_chessboard(char (*a)[8])
 {
 	int c, b;
 
-	for (b = 0; b < 8; b++)
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(a[c][b]);
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[c][b]);
+		}
 	}
 	_putchar('\n');
 }
