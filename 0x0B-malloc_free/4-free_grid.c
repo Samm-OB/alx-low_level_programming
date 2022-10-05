6d34002e1cb3451bf49ee4312e14efa986131ad8
Samuel <smlofoegbu@gmail.com>
2022-10-05 05:48:16 -0700

    malloc free 5

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 /**
+ * free_grid - prints a grid of integers
+ * @grid: an array of 2 dimensional grid
+ * @height: height of grid
+ * Return: nothing
  */
 
 void free_grid(int **grid, int height)
