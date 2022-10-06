bac519d1e873aaebf95fadaf4fa8ae2681367de0
Samuel <smlofoegbu@gmail.com>
2022-10-06 03:10:27 -0700

    memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of memory
+ * Return: 0 for success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	return (a);
+}
