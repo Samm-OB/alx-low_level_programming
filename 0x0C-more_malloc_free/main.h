c56d63a6fc67cbc2f056ef0a7c943b21bd58f07c
Samuel <smlofoegbu@gmail.com>
2022-10-06 14:31:11 -0700

    fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-_puts(char *str);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
