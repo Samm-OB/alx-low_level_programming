bef1cf19cf7bc18ee7c8d1aded613f197060bda8
Samuel <smlofoegbu@gmail.com>
2022-10-10 03:15:13 -0700

    preprocessor files and tasks

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef ABSO
+#define ABSO
+#define ABS(X) (((X) < (0)) ? (-(x)) : (X))
+
+#endif /* ABS */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef SUMM
+#define SUMM
+#define SUMM(x, y) ((x) + (y))
+
+#endif /* SUMM */
