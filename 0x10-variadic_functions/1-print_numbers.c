58487c56bd16f75f63ae2ef7595fdde8f9a14a31
Samuel <smlofoegbu@gmail.com>
2022-10-13 12:19:58 -0700

    prints strings and fixed print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list numbers;
 
+	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - prints strings and new line
+ * @separator: string to be printed btw strings
+ * @n: num of strings
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+	va_list strings;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(strings, char *);
+		if (s == NULL)
+		{
+			s = "(nil)";
+		}
+		printf("%s", s);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(strings);
+}
