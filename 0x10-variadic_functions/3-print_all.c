915341ddaa3e9ca379d11fa6e9846c431a256ad8
Samuel <smlofoegbu@gmail.com>
2022-10-13 12:59:26 -0700

    print all fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,16 +22,16 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", separator, va_args(args, int));
+				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%s%d", separator, va_args(args, int));
+				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%s%f", separator, va_args(args, double));
+				printf("%s%f", separator, va_arg(args, double));
 				break;
 			case 's':
-				s = va_args(args, char *);
+				s = va_arg(args, char *);
 				if (s == NULL)
 				{
 					s = "(nil)";
